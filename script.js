cb5d9da8928ba3fc2a033f28ca0653949d4c4ca0
Venkata Suneel G <suneelvenkat112@gmail.com>
2025-08-24 18:46:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
-// Contact form alert
+// Contact form
 document.getElementById('contact-form').addEventListener('submit', function(e){
   e.preventDefault();
   alert('Thank you! Your message has been sent.');
   this.reset();
 });
-
-// Smooth scroll already enabled via CSS
 
 // Burger menu toggle
 const burger = document.querySelector('.burger');
