1583ca8177e41d0c97ce154401616ac863f7a1e2
Venkata Suneel G <suneelvenkat112@gmail.com>
2025-08-25 15:36:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-// Contact form
-document.getElementById('contact-form').addEventListener('submit', function(e){
-  e.preventDefault();
-  alert('Thank you! Your message has been sent.');
-  this.reset();
-});
-
-// Burger menu toggle
-const burger = document.querySelector('.burger');
-const nav = document.querySelector('nav ul');
-
-burger.addEventListener('click', () => {
-  nav.classList.toggle('active');
-});
 
